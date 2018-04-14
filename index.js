e76a3e638044a1f3e7c46c05eaa203e3639cf636
pranavj1001 <pranavj1001@gmail.com>
2018-04-15 01:55:50 +0530

    set custom height and width of the window

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const { app, BrowserWindow } = electron;
 let mainWindow;
 
 app.on('ready', () => {
-  mainWindow = new BrowserWindow({});
+  mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800
+  });
   mainWindow.loadURL(`file://${__dirname}/index.html`);
 });
