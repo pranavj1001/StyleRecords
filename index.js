e5aeb18657ac0ac57c0ab152f600ae848f34c501
pranavj1001 <pranavj1001@gmail.com>
2018-04-17 23:10:59 +0530

    put html files in templates folder

--- index.js
+++ index.js
@@ -5,9 +5,6 @@ const { app, BrowserWindow } = electron;
 let mainWindow;
 
 app.on('ready', () => {
-  mainWindow = new BrowserWindow({
-    width: 1200,
-    height: 800
-  });
-  mainWindow.loadURL(`file://${__dirname}/index.html`);
+  mainWindow = new BrowserWindow({ width: 1200, height: 800 });
+  mainWindow.loadURL(`file://${__dirname}/templates/home.html`);
 });
