457e3df732657d636ffba86d22abb57ed00af044
pranavj1001 <pranavj1001@gmail.com>
2018-04-17 23:47:33 +0530

    add logic to open newNote window

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const electron = require('electron');
 
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, ipcMain } = electron;
 
 let mainWindow;
 
 app.on('ready', () => {
   mainWindow = new BrowserWindow({ width: 1200, height: 800 });
   mainWindow.loadURL(`file://${__dirname}/templates/home.html`);
+});
+
+ipcMain.on('note: add', () => {
+  mainWindow = new BrowserWindow({ width: 1200, height: 800 });
+  mainWindow.loadURL(`file://${__dirname}/templates/newNote.html`);
 });
