0c24daf235a20a5077cfe664f75b243834e29823
pranavj1001 <pranavj1001@gmail.com>
2018-04-18 00:35:58 +0530

    load html to current window
    instead of openning a new window, load html to current window

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
 const electron = require('electron');
 
-const { app, BrowserWindow, ipcMain } = electron;
+const { app, BrowserWindow } = electron;
 
 let mainWindow;
 
 app.on('ready', () => {
   mainWindow = new BrowserWindow({ width: 1200, height: 800 });
   mainWindow.loadURL(`file://${__dirname}/templates/home.html`);
-});
-
-ipcMain.on('note: add', () => {
-  mainWindow = new BrowserWindow({ width: 1200, height: 800 });
-  mainWindow.loadURL(`file://${__dirname}/templates/newNote.html`);
 });
