1589f77b4a6bb5859a153c42a2b0711053c10ac8
pranavj1001 <pranavj1001@gmail.com>
2018-04-19 23:30:08 +0530

    establish a connection to db

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const electron = require('electron');
+const mongoose = require('mongoose');
 
 const { app, BrowserWindow } = electron;
 
 let mainWindow;
+
+mongoose.connect('mongodb://localhost/style_records');
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('Connection established');
+});
 
 app.on('ready', () => {
   mainWindow = new BrowserWindow({ width: 1200, height: 800 });
