b4e14a1b4520f1846dad03f40831ae18e00e3b45
pranavj1001 <pranavj1001@gmail.com>
2018-04-24 16:16:10 +0530

    fix constants

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const electron = require('electron');
 const mongoose = require('mongoose');
 const Record = require('./database/models/record');
-const { RECORD_SAVED } = require('./constants');
+const {
+  RECORD_SAVED_CONFIRMATION_MESSAGE,
+  REQUEST_TO_SAVE_RECORD,
+  RECORD_SAVED_CONFIRMATION,
+  RECORDS_FETCH_CONFIRMATION,
+  REQUEST_TO_FETCH_ALL_RECORDS } = require('./constants');
 
 const { app, BrowserWindow, ipcMain } = electron;
 
@@ -12,7 +17,7 @@ mongoose.Promise = global.Promise;
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 
-ipcMain.on('record: save', (event, recordData) => {
+ipcMain.on(REQUEST_TO_SAVE_RECORD, (event, recordData) => {
   const newRecord = new Record({
     title: recordData.title,
     author: recordData.author,
@@ -22,16 +27,18 @@ ipcMain.on('record: save', (event, recordData) => {
    });
   newRecord.save()
     .then(() => {
-      mainWindow.webContents.send('record: saved_confirmation', RECORD_SAVED);
+      mainWindow.webContents.send(RECORD_SAVED_CONFIRMATION, RECORD_SAVED_CONFIRMATION_MESSAGE);
+    });
+});
+
+ipcMain.on(REQUEST_TO_FETCH_ALL_RECORDS, () => {
+  Record.find({})
+    .then((fetchedRecords) => {
+      mainWindow.webContents.send(RECORDS_FETCH_CONFIRMATION, fetchedRecords);
     });
 });
 
 app.on('ready', () => {
   mainWindow = new BrowserWindow({ width: 1200, height: 800 });
   mainWindow.loadURL(`file://${__dirname}/templates/home.html`);
-
-  Record.find({})
-    .then((fetchedRecords) => {
-      mainWindow.webContents.send('record: fetch_all', fetchedRecords);
-    });
 });
