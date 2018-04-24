ab62ede686c75f427f93b1dc6dcdc703309609f6
pranavj1001 <pranavj1001@gmail.com>
2018-04-24 16:08:13 +0530

    update constant.js

--- constants.js
+++ constants.js
@@ -4,7 +4,8 @@ module.exports.RECORD_EDITED_CONFIRMATION_MESSAGE = 'Your record has been edited
 
 // requests sent by mainWindow to mainProcess
 module.exports.REQUEST_TO_SAVE_RECORD = 'record: save';
+module.exports.REQUEST_TO_FETCH_ALL_RECORDS = 'record: fetch_all';
 
 // data sent by mainProcess to mainWindow
 module.exports.RECORD_SAVED_CONFIRMATION = 'record: saved_confirmation';
-module.exports.RECORDS_FETCH_CONFIRMATION = 'record: fetch_all';
+module.exports.RECORDS_FETCH_CONFIRMATION = 'record: fetched_confirmation';
