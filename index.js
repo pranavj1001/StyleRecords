8b30d1cef687b5b9d6e7a3e8edc4ea05f631cbb8
pranavj1001 <pranavj1001@gmail.com>
2018-04-26 23:26:10 +0530

    prepare logic to load record details to edit it

--- index.js
+++ index.js
@@ -6,15 +6,19 @@ const {
   REQUEST_TO_SAVE_RECORD,
   REQUEST_TO_SAVE_ID_TEMP,
   REQUEST_TO_FETCH_A_RECORD,
+  REQUEST_TO_EDIT_A_RECORD,
+  REQUEST_TO_FETCH_ALL_RECORDS,
+  REQUEST_TO_CHECK_FOR_EDIT_REQUEST,
   RECORD_SAVED_CONFIRMATION,
   RECORD_FETCH_CONFIRMATION,
   RECORDS_FETCH_CONFIRMATION,
-  REQUEST_TO_FETCH_ALL_RECORDS } = require('./constants');
+  RECORD_EDIT_PENDING_REQUEST } = require('./constants');
 
 const { app, BrowserWindow, ipcMain } = electron;
 
 let mainWindow;
 let currentOpenRecord;
+let editRequest;
 
 mongoose.connect('mongodb://localhost/style_records');
 mongoose.Promise = global.Promise;
@@ -63,6 +67,28 @@ ipcMain.on(REQUEST_TO_FETCH_A_RECORD, () => {
     .then((record) => {
       mainWindow.webContents.send(RECORD_FETCH_CONFIRMATION, record);
     });
+});
+
+ipcMain.on(REQUEST_TO_EDIT_A_RECORD, () => {
+  editRequest = true;
+});
+
+ipcMain.on(REQUEST_TO_CHECK_FOR_EDIT_REQUEST, () => {
+  if (editRequest) {
+    editRequest = false;
+    Record.findById(currentOpenRecord)
+      .then((fetchedRecord) => {
+        const record = {
+          title: fetchedRecord.title,
+          author: fetchedRecord.author,
+          keywords: fetchedRecord.keywords,
+          content: fetchedRecord.content,
+          date: fetchedRecord.date,
+          id: fetchedRecord._id.toString()
+        };
+        mainWindow.webContents.send(RECORD_EDIT_PENDING_REQUEST, record);
+      });
+  }
 });
 
 app.on('ready', () => {
